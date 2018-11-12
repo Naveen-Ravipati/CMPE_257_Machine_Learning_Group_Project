d344ca5048e53453db811347d6169311983859a6
Sachin Guruswamy <saching12@Sachins-MBP.hsd1.ca.comcast.net>
2018-11-11 16:50:11 -0800

    preprocessing

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
-#SVM and Random Forest
-#Team Project
+# SVM and Random Forest
+# Team Project
+
+import numpy as np
+import pandas as pd
+import re
+
+
+def char_flag(l):
+    if l <= 40:
+        return 1
+    elif l <= 60:
+        return 2
+    elif l <= 80:
+        return 3
+    elif l <= 120:
+        return 4
+    elif l <= 160:
+        return 5
+    else:
+        return 6
+
+
+df = pd.read_csv("spam.csv", encoding='latin-1')
+df = df.drop(columns=['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'])
+print(df.head(5))
+
+df_data = df['v2']
+token_1 = []
+token_2 = []
+token_3 = []
+token_4 = []
+token_5 = []
+token_6 = []
+
+for d in df_data:
+    token_1.append(re.sub('[^A-Za-z]+', '', d))
+    token_2.append(d.count('$'))
+    token_3.append(re.sub('[^0-9]+', '', d))
+    token_4.append(len(d))
+    token_5.append(char_flag(len(d)))
